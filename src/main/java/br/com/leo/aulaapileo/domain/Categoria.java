09c6d8c90143c6caf6afe4a8b95be2a73d6bf9fb
Aluno <aluno@eteclencois.com.br>
2019-03-28 11:37:15 -0300

    criação do relacionamento entre as tabelas e produtos

--- src/main/java/br/com/leo/aulaapileo/domain/Categoria.java
+++ src/main/java/br/com/leo/aulaapileo/domain/Categoria.java
@@ -1,11 +1,14 @@
 package br.com.leo.aulaapileo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 
 @Entity
 public class Categoria implements Serializable{
@@ -17,6 +20,8 @@ public class Categoria implements Serializable{
 	private Integer id;
 	private String nome;
 
+	@OneToMany(mappedBy = "categoria")
+	private List<Produto> produtos = new ArrayList<>();
 	public Categoria() {
 		
 	}
--- src/main/java/br/com/leo/aulaapileo/domain/Produto.java
+++ src/main/java/br/com/leo/aulaapileo/domain/Produto.java
@@ -8,6 +8,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 
 @Entity
 public class Produto implements Serializable{
@@ -20,16 +22,23 @@ public class Produto implements Serializable{
 	private String nome;
 	private BigDecimal preco;
 
+	@ManyToOne
+	@JoinColumn(name = "categoria_id")
+	private Categoria categoria;
+	
 	public Produto() {
 		
 	}
 
-	public Produto(Integer id, String nome, BigDecimal preco) {
+	
+	public Produto(Integer id, String nome, BigDecimal preco, Categoria categoria) {
 		super();
 		this.id = id;
 		this.nome = nome;
 		this.preco = preco;
+		this.categoria = categoria;
 	}
+
 
 	public Integer getId() {
 		return id;
@@ -53,7 +62,19 @@ public class Produto implements Serializable{
 
 	public void setPreco(BigDecimal preco) {
 		this.preco = preco;
+		
 	}
+	
+
+	public Categoria getCategoria() {
+		return categoria;
+	}
+
+
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	}
+
 
 	@Override
 	public int hashCode() {
